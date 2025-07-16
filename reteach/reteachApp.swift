9e75eb3648b7c32ea1adeb272bd2839d4851caf6
yummyweb <antriksh.timepass@gmail.com>
2025-07-16 15:28:37 +0530

    Initial Commit

---
+++ reteach/reteachApp.swift
@@ -0,0 +1,17 @@
+//
+//  reteachApp.swift
+//  reteach
+//
+//  Created by Antariksh Verma on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct reteachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ reteachTests/reteachTests.swift
@@ -0,0 +1,17 @@
+//
+//  reteachTests.swift
+//  reteachTests
+//
+//  Created by Antariksh Verma on 16/07/25.
+//
+
+import Testing
+@testable import reteach
+
+struct reteachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
